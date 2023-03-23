cef2c1d2f61ce75d854e296c3b54442a51369921
praneeth376 <120455504+praneeth376@users.noreply.github.com>
2023-03-23 12:03:10 +0530

    Create calender.py

---
+++ calender.py
@@ -0,0 +1,11 @@
+# Python code to demonstrate the working of
+# calendar() function to print calendar
+
+# importing calendar module
+# for calendar operations
+import calendar
+
+# using calendar to print calendar of year
+# prints calendar of 2018
+print ("The calendar of year 2023 is : ")
+print (calendar.calendar(2023))
